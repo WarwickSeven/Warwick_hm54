91314c3eb6a5e2d59fb5fe62f4cc064335e1f0b7
WarwickSeven <WarwickSeven@gmail.com>
2022-02-28 17:56:26 +0300

    Initial Commit

---
+++ Warwick_hm54/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Warwick_hm54
+//
+//  Created by Александр Тихонов on 28.02.2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
